cd8acdeece464dfdaf13a7421a0f069b44ef5dbb
Fabio Scaccabarozzi <fsvm88@gmail.com>
2024-10-05 16:07:31 +0200

    resort script: skip symlinks processing

--- resort_stuff.sh
+++ resort_stuff.sh
@@ -3,6 +3,7 @@
 for a in $(find -type d -name "package.*" ! -empty) "sets"; do
     cd "${a}"
     for b in *; do
+        [ -L "$b" ] && continue
         sort -u "${b}" > asd
         mv asd "${b}"
     done
